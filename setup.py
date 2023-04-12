f9eb7cbdb8814af1608c2a99a6a78327fdd83139
Perfectfire33 <perfectfireiii@gmail.com>
2023-04-11 21:18:48 -0400

    Updated Support Page
    Added what different donations can provide

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flaskrOpenHousePartyWebsite',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
