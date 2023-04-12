18fd9ae66d8c7293afcc7a9c768ac605250eee8d
Perfectfire33 <perfectfireiii@gmail.com>
2023-04-11 21:22:30 -0400

    Updated Version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flaskrOpenHousePartyWebsite',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
