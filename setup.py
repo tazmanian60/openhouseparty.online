391652671bf49f100b3fdab99a6cfbf22c606f91
Perfectfire33 <perfectfireiii@gmail.com>
2023-07-18 15:16:37 -0400

    Added Zeffy link to Store page

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flaskrOpenHousePartyWebsite',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
