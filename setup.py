dcb66777782c2a1f619ea8d2d678eacd51038492
Perfectfire33 <perfectfireiii@gmail.com>
2023-07-18 15:50:08 -0400

    Update to V 1.0.8
    Changes from last commit didn't work. Turned out to be a waitress issue. Updating for 1.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flaskrOpenHousePartyWebsite',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
