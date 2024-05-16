24730bed5f167f71267f9ea025a94fe52a94e6b2
Perfectfire33 <perfectfireiii@gmail.com>
2024-05-16 18:50:17 -0400

    Update to V1.0.11
    Just version number change.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flaskrOpenHousePartyWebsite',
-    version='1.0.10',
+    version='1.0.11',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
